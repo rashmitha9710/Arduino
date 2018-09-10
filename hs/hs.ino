ff4bffac043392ad9bc49fc7a3e962d3da14b3e3
Futseh <jonas.folvik@gmail.com>
2018-09-10 08:46:48 +0200

    changes

--- hs/hs.ino
+++ hs/hs.ino
@@ -1,9 +1,20 @@
+int num = 0;
+
 void setup()
 {
-  
+  Serial.begin(9600);
 }
 
 void loop()
+{ 
+  Serial.print("Dette programmet har kjørt ");
+  num++;
+  String txt = write(String(num));
+  Serial.println(txt);
+  delay(50);
+}
+
+String write(String text)
 {
-  
+  return text + " ganger!";
 }
